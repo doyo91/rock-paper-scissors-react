b6644188517b5dd453010f7883608f3972da1d13
doyo91 <doyo_91@hotmail.com>
2020-07-17 16:48:35 +0200

    Updates

---
+++ precache-manifest.9d49322dbed402a0e69735ab415c7f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033dd91660b4909a5342db6baa454303",
+    "url": "/rock-paper-scissors-react/index.html"
+  },
+  {
+    "revision": "419559aa624e1a132582",
+    "url": "/rock-paper-scissors-react/static/css/main.dac3a658.chunk.css"
+  },
+  {
+    "revision": "15951dfd85ce232c6db5",
+    "url": "/rock-paper-scissors-react/static/js/2.4aa9adca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors-react/static/js/2.4aa9adca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419559aa624e1a132582",
+    "url": "/rock-paper-scissors-react/static/js/main.9ded7ec9.chunk.js"
+  },
+  {
+    "revision": "8b7f9d9fa81518556f46",
+    "url": "/rock-paper-scissors-react/static/js/runtime-main.f2da3a22.js"
+  }
+]);
